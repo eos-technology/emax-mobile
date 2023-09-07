ae75552f04c29c2005f27158732aba3e7d466c20
Camilo Gonzalez <camilogonzalez@MacBook-Pro-de-Camilo.local>
2023-09-07 11:36:40 -0500

    adding initial congiguration

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hola mundo</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
